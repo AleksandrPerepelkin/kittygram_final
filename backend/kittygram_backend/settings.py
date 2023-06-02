034c9fb3cccbb1196ad966e5b76c100cee27f537
AleksandrPerepelkin <wuld@mail.ru>
2023-06-02 23:09:24 +0300

    02/06

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -104,8 +104,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/www/kittygram/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = '/var/www/kittygram/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
