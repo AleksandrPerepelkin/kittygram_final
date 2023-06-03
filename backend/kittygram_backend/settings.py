5ba73b21606f146e62d5bbccd1e4e55fd0c03c3e
AleksandrPerepelkin <wuld@mail.ru>
2023-06-03 16:09:27 +0300

    03/06

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,9 +2,13 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECRET_KEY = os.getenv('DJANGO_SECRET_KEY') не получилось у меня
 # сделать по красоте и код запихнуть в контейнер,
@@ -101,11 +105,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = '/var/www/kittygram/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
